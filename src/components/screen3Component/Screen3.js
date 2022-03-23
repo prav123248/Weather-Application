8b9cda8a2cfad3181149f248ba9654b66239b628
Pravin Prabhakaran <pravin.prabha123248@gmail.com>
2022-03-23 16:44:04 +0000

    screen 3 finished

--- src/components/screen3Component/Screen3.js
+++ src/components/screen3Component/Screen3.js
@@ -3,6 +3,7 @@ import '../../styles/common.css'
 import Header from '../headerComponent/header';
 import Toolbar from '../toolbarComponent/toolbar';
 import './style.css'
+import $ from 'jquery';
 
 //Icon imports
 import cloudy from '../../assets/icons/cloudy.png'
@@ -15,111 +16,138 @@ import direction from '../../assets/icons/direction.png'
 export default class Screen3 extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            trail: ""
+        }
+
     }
+
+
+    scheduledDescription = () => {
+        if (this.state.trail === "") {
+            return (
+                <div class="scheduleDetailContainer">
+                    <p class="scheduleMessage">Select a trail to view details or update the forecast</p>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div class="scheduleDetailContainer">
+                    <h3>Trail : {this.state.trail}</h3>
+                    <p class="scheduleForecast scheduleMessage">Last Checked : <img src={snow}/></p>
+                    <div id="buttonContainer">
+                        <button id="updateButton" type="button">Update Forecast</button>
+                    </div>
+                </div>
+            )
+        }
+    }
+
+
+    scheduleOnClick = () => {
+        const self = this;
+        $('.scheduledRow').on('click', function () {
+            var el = $(this).children('.scheduleName').text();
+            if (el !== "Trail Name") {
+                self.changeTrail(el)
+            }
+        });        
+    }
+
+    changeTrail = (selectedTrail) => {
+        this.setState({
+            trail:selectedTrail
+        })
+    }
+
+    componentDidMount() {
+        this.scheduleOnClick();
+        
+    }
+
     render(){
         return (
                 
             <div id="screen3" className="container">   
                 <Header />
-                <div class="upperScrollContainer">
+                <div class="scheduleContainer">
                     <h2>Trail Schedule</h2>
-                    <div class="scrollableTable">
+                    <div class="scheduleTable">
                         <table>
                             <tbody>
-                                <tr>
-                                    <th class="scheduleName">Trail Name</th>
+                                <tr class="scheduledRow">
                                     <th class="scheduleDate">Date</th>
-                                    <th class="scheduleCondition">Condition</th>
+                                    <th class="scheduleName">Trail Name</th>
+                                    <th class="scheduleForecast">Forecast</th>
                                 </tr>
 
-                                <tr>
+                                <tr class="scheduledRow">
+                                    <td class="scheduleDate">23/01/21</td>
                                     <td class="scheduleName">Harrow</td>
-                                    <td class="scheduleDate">23/01/21</td>
-                                    <td><img src={snow}/></td>
+                                    <td class="scheduleForecast"><img src={snow}/></td>
                                 </tr>   
 
-                                <tr>
-                                    <td class="scheduleName">Britain</td>
+                                <tr class="scheduledRow">
                                     <td class="scheduleDate">23/01/21</td>
+                                    <td class="scheduleName">Harrow</td>
+                                    <td class="scheduleForecast"><img src={snow}/></td>
                                 </tr>   
-                                <tr>
+                                <tr class="scheduledRow">
+                                    <td class="scheduleDate">23/01/21</td>
                                     <td class="scheduleName">Britain and some other land </td>
-                                    <td class="scheduleDate">23/01/21</td>
+                                    <td class="scheduleForecast"><img src={snow}/></td>
                                 </tr>   
 
-                                <tr>
+                                <tr class="scheduledRow">
+                                    <td class="scheduleDate">23/01/21</td>
                                     <td class="scheduleName">asdasdadausdh aushd asu hdasduiash duas daisu daihd asihd isuhd asuihdaishdiaushdisaBritain and some other land </td>
-                                    <td class="scheduleDate">23/01/21</td>
+                                    <td class="scheduleForecast"><img src={snow}/></td>
                                 </tr> 
 
-                                <tr>
-                                    <td class="scheduleName">Britain and some other land </td>
+                                <tr class="scheduledRow">
                                     <td class="scheduleDate">23/01/21</td>
-                                </tr> 
+                                    <td class="scheduleName">Britain and some other land </td>
+                                    <td class="scheduleForecast"><img src={snow}/></td>
+                                </tr>   
 
-                                <tr>
-                                    <td class="scheduleName">Britain and some other land </td>
+                                <tr class="scheduledRow">
                                     <td class="scheduleDate">23/01/21</td>
-                                </tr> 
+                                    <td class="scheduleName">Britain and some other land </td>
+                                    <td class="scheduleForecast"><img src={snow}/></td>
+                                </tr>   
 
-                                <tr>
-                                    <td class="scheduleName">Britain and some other land </td>
+                                <tr class="scheduledRow">
                                     <td class="scheduleDate">23/01/21</td>
-                                </tr> 
+                                    <td class="scheduleName">Britain and some other land </td>
+                                    <td class="scheduleForecast"><img src={snow}/></td>
+                                </tr>   
 
-                                <tr>
-                                    <td class="scheduleName">Britain and some other land </td>
+                                <tr class="scheduledRow">
                                     <td class="scheduleDate">23/01/21</td>
-                                </tr> 
+                                    <td class="scheduleName">Britain and some other land </td>
+                                    <td class="scheduleForecast"><img src={snow}/></td>
+                                </tr>   
 
-                                <tr>
-                                    <td class="scheduleName">Britain and some other land </td>
+                                <tr class="scheduledRow">
                                     <td class="scheduleDate">23/01/21</td>
-                                </tr> 
+                                    <td class="scheduleName">Britain and some other land </td>
+                                    <td class="scheduleForecast"><img src={snow}/></td>
+                                </tr>   
 
-                                <tr>
-                                    <td class="scheduleName">Britain and some other land </td>
+                                <tr class="scheduledRow">
                                     <td class="scheduleDate">23/01/21</td>
-                                </tr> 
+                                    <td class="scheduleName">Britain and some other land </td>
+                                    <td class="scheduleForecast"><img src={sunny}/></td>
+                                </tr>   
                             </tbody>
                         </table>
 
                     </div>
                 </div>
                 
-                <div class="addSchedule">
+                {this.scheduledDescription()}
 
-
-                    <h3>Schedule a Hike</h3>
-                    <div class="formContainer">
-                        <div class="smallPadding scheduleSelecter">
-                            <select name="trails">
-                                <option value="" selected="selected" disabled>Select Trail</option>
-                                <option value="London">London</option>
-                                <option value="London">London</option>
-                                <option value="London">London</option>
-                                <option value="London">London</option>
-                                <option value="London">London</option>
-                                <option value="London">London</option>
-                                <option value="London">London</option>
-                                <option value="London">London</option>
-                                <option value="London">London</option>
-                                <option value="London">London</option>
-                                <option value="London">London</option>
-                                <option value="London">London</option>
-                            </select>
-                        </div>
-
-                        <div class="smallPadding">
-                            <input type="date" name="scheduledDate"/>
-                        </div>
-
-                        <input id="scheduleSubmit" type="submit" value="Submit"/>
-                    </div>
-
-                        
-
-                </div>
 
 
                 <Toolbar instance={this.props.instance} screen={3} />
